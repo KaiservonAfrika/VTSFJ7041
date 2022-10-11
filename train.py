c38b5e6f8f4fc31b0b3a22f00209e1f98c6fcc82
KaiservonAfrika <44156190+KaiservonAfrika@users.noreply.github.com>
2022-10-11 19:05:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -230,14 +230,14 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
         evaluate(hps, net_g, eval_loader, writer_eval)
         utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
         utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
-        shutil.copyfile(os.path.join(hps.model_dir, "D_{}.pth".format(global_step)), "/content/drive/My Drive/NewestD.pth")
-        shutil.copyfile(os.path.join(hps.model_dir, "G_{}.pth".format(global_step)), "/content/drive/My Drive/NewestG.pth")
-#        old_g=os.path.join(hps.model_dir, "G_{}.pth".format(global_step-2000))
-#        old_d=os.path.join(hps.model_dir, "D_{}.pth".format(global_step-2000))
-#        if os.path.exists(old_g):
-#          os.remove(old_g)
-#        if os.path.exists(old_d):
-#          os.remove(old_d)
+#        shutil.copyfile(os.path.join(hps.model_dir, "D_{}.pth".format(global_step)), "/content/drive/My Drive/tempmodel/NewestD.pth")
+#        shutil.copyfile(os.path.join(hps.model_dir, "G_{}.pth".format(global_step)), "/content/drive/My Drive/tempmodel/NewestG.pth")
+        old_g=os.path.join(hps.model_dir, "G_{}.pth".format(global_step-2000))
+        old_d=os.path.join(hps.model_dir, "D_{}.pth".format(global_step-2000))
+        if os.path.exists(old_g):
+          os.remove(old_g)
+        if os.path.exists(old_d):
+          os.remove(old_d)
     global_step += 1
   
   if rank == 0:
